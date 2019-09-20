0c5157ff81543e6eb8a1c8ff66bbf4724e731d78
calvince <calmosh1421@gmail.com>
2019-09-20 13:51:41 +0300

    this commit will add all typescript files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import {  FormsModule } from '@angular/forms'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    HighlightDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
